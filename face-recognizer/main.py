476c9156647cb85db5020e1b01940b83aee34023
harsh0code <harshkumar06051974@gmail.com>
2025-06-22 10:57:32 +0530

    Add files via upload

---
+++ face-recognizer/main.py
@@ -0,0 +1,40 @@
+import cv2
+import os
+import numpy as np
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+dataset_path = "dataset"
+
+faces, labels, label_names = [], [], []
+
+for label, person in enumerate(os.listdir("dataset")):
+    label_names.append(person)
+    
+    for img_name in os.listdir(f"dataset/{person}"):
+        img = cv2.imread(f"dataset/{person}/{img_name}", cv2.IMREAD_GRAYSCALE)
+        
+        if img is not None:
+            faces.append(cv2.resize(img, (160, 160)))
+            labels.append(label)
+
+faces_array = np.array(faces)
+labels_array = np.array(labels)
+
+recognizer.train(faces_array, labels_array)
+
+test_img_path = "download.jpg"
+
+test_img = cv2.imread(test_img_path, cv2.IMREAD_GRAYSCALE)
+
+if test_img is None:
+    print("❌ Error: Could not read test image. Check path and file name.")
+    exit()
+    
+
+test_img_resized = cv2.resize(test_img, (160, 160))
+
+label, confidence = recognizer.predict(test_img_resized)
+predicted_name = label_names[label]
+
+print(f"✅ Predicted: {predicted_name} with confidence {confidence}")
